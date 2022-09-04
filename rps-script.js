fa420e7cf7b27ff4a75f47de29378faca2177b17
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-09-04 15:16:07 -0700

    UI functionality

--- rps-script.js
+++ rps-script.js
@@ -10,26 +10,10 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let player_choice = prompt('Rock, paper, or scissors?')
-    switch (player_choice.toUpperCase()) {
-        case 'ROCK':
-        case 'R':
-            return 'R'
-        case 'PAPER':
-        case 'P':
-            return 'P'
-        case 'SCISSORS':
-        case 'S':
-            return 'S'
-        default:
-            return false
-    }
-}
-
-function playGame(player_choice, computer_choice) {
-    let result
-
+function playGame(player_choice) {
+    let computer_choice = getComputerChoice()
+    let result;
+    let dict = {R: 'Rock', P: 'Paper', S: 'Scissors'};
     if (player_choice === computer_choice) {
         result = "It's a tie!"
     } else if ((player_choice === 'R' && computer_choice === 'P') || (player_choice === 'P' && computer_choice === 'S') || (player_choice === 'S' && computer_choice === 'R')) {
@@ -37,18 +21,12 @@ function playGame(player_choice, computer_choice) {
     } else if ((player_choice === 'P' && computer_choice === 'R') || (player_choice === 'S' && computer_choice === 'P') || (player_choice === 'R' && computer_choice === 'S')) {
         result = 'You won!'
     }
-    return `Your choice: ${player_choice}\nComputer choice: ${computer_choice}\n${result}`
+    alert(`Your choice: ${dict[player_choice]}\nComputer choice: ${dict[computer_choice]}\n${result}`)
 }
 
-let player_choice
-let computer_choice
-
-while (true) {
-    player_choice = getPlayerChoice()
-    if (!player_choice) {
-        alert('Invalid input! Try again.')
-    } else {
-        computer_choice = getComputerChoice()
-        alert(playGame(player_choice, computer_choice))
-    }
-}
+r_button = document.querySelector('.rock')
+r_button.addEventListener('click', () => playGame('R'))
+p_button = document.querySelector('.paper')
+p_button.addEventListener('click', () => playGame('P'))
+s_button = document.querySelector('.scissors')
+s_button.addEventListener('click', () => playGame('S'))
